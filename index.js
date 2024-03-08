f380292f3de6a8af02161f1749a01913976b2358
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:07:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// 3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+var name1 = "uzma imran";
+// uppercase
+console.log(name1.toUpperCase());
+// lowercase
+console.log(name1.toLowerCase());
+// titlecase
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1));
